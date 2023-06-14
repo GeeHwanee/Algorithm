ea54f1a21f40ebc7df4d7b32d6cf93ed5ae4f4ee
JiHwan Park <120438094+GeeHwanee@users.noreply.github.com>
2023-06-14 14:35:20 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String c = br.readLine();
+        char ch = c.charAt(0);
+        System.out.println((int)ch);
+    }
+}
