2b4ad71c1cfb39d60cead1a536afdb6e74241b02
JiHwan Park <120438094+GeeHwanee@users.noreply.github.com>
2023-06-14 18:44:28 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int day = 2541-1998;
+        Scanner scanner = new Scanner(System.in);
+        int y = scanner.nextInt();
+        System.out.println(y-day);
+    }
+}
