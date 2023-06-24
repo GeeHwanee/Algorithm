b0c0888715b4aec9409b4fd72a7429adfb57cbd7
JiHwan Park <120438094+GeeHwanee@users.noreply.github.com>
2023-06-24 11:02:45 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2876 ms, Memory: 386420 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(bufferedReader.readLine());
+        }
+       int[] newList = Arrays.stream(list).sorted().toArray();
+        for (int i = 0; i < N; i++) {
+            bufferedWriter.write(Integer.toString(newList[i])+"\n");
+        }
+        bufferedWriter.close();
+    }
+}
